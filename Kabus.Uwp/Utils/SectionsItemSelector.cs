a16a96abfbbeab2bee71bd8714be7b126089697a
Kirk Woll <kirk.woll@gmail.com>
2018-09-03 15:46:23 -0700

    Introduce GridSplitter

---
+++ Kabus.Uwp/Utils/SectionsItemSelector.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Kabus.Uwp.ViewModels;
+
+namespace Kabus.Uwp.Utils
+{
+    public class SectionsItemSelector : DataTemplateSelector
+    {
+        public DataTemplate SectionTemplate { get; set; }
+        public DataTemplate FolderTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            if (item is SectionViewModel)
+                return SectionTemplate;
+            else
+                return FolderTemplate;
+        }
+    }
+}
